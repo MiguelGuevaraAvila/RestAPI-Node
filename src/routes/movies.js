313924b59433587677bd95aa6c6291d96f436677
MiguelGuevara <42946893+MiguelGuevaraAvila@users.noreply.github.com>
2023-04-13 22:59:39 -0500

    Changes
    Pushing new movie

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -8,4 +8,25 @@ router.get('/', (req, res) => {
         res.json(movies);
 });
 
+router.post('/', (req, res) => {
+    //console.log(req.body);
+    const {title, director, year, type}=req.body    
+    if(title && director && year && type)
+    {
+        const id = movies.length+1
+        const newMovie ={id, ...req.body};
+        res.json('saved');
+        console.log(newMovie);
+        //movies.push();
+    }
+    else{
+        res.send('Wrong request');
+    }
+
+    //res.send('received');
+});
+
 module.exports = router;
+
+// 46:00 minutos
+//https://www.youtube.com/watch?v=bK3AJfs7qNY&list=PLTzdf6VDuldGEgyhQFKzTDXRQQ3lMJUoQ&index=3
