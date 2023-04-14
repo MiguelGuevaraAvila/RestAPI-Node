351e7238090afc41988db117357528b3f3af4410
MiguelGuevara <42946893+MiguelGuevaraAvila@users.noreply.github.com>
2023-04-14 00:44:18 -0500

    Change
    Methods delete and put  implemented

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,5 +1,6 @@
 const { Router} = require('express');
 const router = Router();
+const _ = require('underscore');
 
 const movies = require('./sample.json');
 //console.log(movies);
@@ -10,22 +11,55 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     //console.log(req.body);
-    const {title, director, year, type}=req.body    
+    const {title, director, year, type}= req.body;
     if(title && director && year && type)
     {
-        const id = movies.length+1
-        const newMovie ={id, ...req.body};
-        res.json('saved');
-        console.log(newMovie);
-        //movies.push();
+        const id = movies.length + 1;
+        const newMovie = {...req.body, id};
+        res.json(movies);
+        //console.log(newMovie);
+        movies.push(newMovie);
+
+        //Validate form
+        //Use Express.validator
+        //https://express-validator.github.io/docs/
     }
     else{
-        res.send('Wrong request');
+        //res.send('Wrong request');
+        res.status(500).json({error: 'There was an error'});
     }
-
-    //res.send('received');
 });
 
+router.delete('/:id', (req, res) => {
+    const { id} =req.params; 
+    _.each(movies, (movie, i)=>{
+        if(movie.id == id)
+        {
+            movies.splice(i, 1);
+        }
+    });
+    //console.log(req.params);
+    res.send(movies);
+});
+
+router.put('/:id',(req, res)=>{
+    const {id} =req.params; // url params
+    const { title, year, director, type } = req.body; //data body
+    if(title && director && year && type){
+        _.each(movies, (movie, i)=>{
+            if(movie.id == id){
+                movie.title= title;
+                movie.year = year;
+                movie.director = director;
+                movie.type = type;
+            }
+            res.json(movies);
+        })
+    }
+    else{
+        res.status(500).json('all data are required');
+    }
+});
 module.exports = router;
 
 // 46:00 minutos
